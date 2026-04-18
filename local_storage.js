11167ba0e1ac0eb9c31fc2c398343535adfedcdf
G.P. <gpvanzyl3@gmail.com>
2026-04-18 16:01:11 +0200

    add a page to create person and remove (will update file name) add a page for admin login and one for project

--- local_storage.js
+++ local_storage.js
@@ -3,7 +3,8 @@ let projects =
 [
     {
         id:0,
-        name: 'UI'
+        name: 'UI',
+        details:"To make the UI for the website"
     },
     {},
 ]    
@@ -14,7 +15,7 @@ let people = [
         FirstName:"Jack",
         LastName:"Wiilem",
         email:"jack.w@gmail.com",
-        username:"123Jack"
+        username:"Jac123"
     },
     {}
 ]
@@ -32,7 +33,78 @@ let issues = [
     {}
 ]
 
+let admin = {
+    get uname() {
+        return people[0].username;
+    } ,
+    get email() {
+        return people[0].email;
+    },
+    password: "Jack1234"
+}
+
 //adds the arrays to local storage
-calStorage.setItem('projects', JSON.stringify(projects));
+localStorage.setItem('projects', JSON.stringify(projects));
 localStorage.setItem('people', JSON.stringify(people));
 localStorage.setItem('issues', JSON.stringify(issues));
+localStorage.setItem('admin',JSON.stringify(admin));
+
+// Adds person to local storage
+
+function addingperson() {
+    let personForm = document.getElementById(`personForm`);
+    
+    personForm.addEventListener('submit', (e) => {
+        e.preventDefault(); //stops refreshing
+    
+        // Getting the data from the form
+        let formdata = new FormData(personForm) //Instead of getting each input manually formdata takes all input elements and we can then just use it 
+    
+        let FirstName = formdata.get('fname'),
+        LastName = formdata.get('lname'),
+        email = formdata.get('email'),
+        username = FirstName.slice(0,3) + "123"
+        let newdata = {
+            FirstName: FirstName,
+            LastName: LastName,
+            email: email,
+            username: username
+        };
+    
+        people.push(newdata);
+        localStorage.setItem('people',JSON.stringify(people))
+    })
+}
+
+function addingproject() {
+    
+}
+
+function checkingAdmin()
+{
+    // let admin = JSON.parse(localStorage.getItem('admin'));
+    let input = {uname: document.getElementById('uname').value,
+        email:document.getElementById('email').value,
+        password: document.getElementById('password').value};
+    let message = document.getElementById('message');
+    let test = document.getElementById('test')
+        test.innerText=input.uname, admin.uname;
+    if(!(input.password==admin.password)) {
+    }
+    else {
+        if(!(input.uname === admin.username)){
+            message.innerText = 'wrong username. Try again'
+        }
+        else{
+            if(!(input.email===admin.email))
+                {
+                message.innerText = 'wrong email. Try again'
+            }
+            else{
+                message.innerText = 'welcome'
+            }
+        }
+    }
+}
+
+addingperson();
